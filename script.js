0871fbe2d0d2ea79dd2e33882cccc6c21bcb1a3d
adrian <adriangallanoengr@gmail.com>
2022-02-13 18:12:59 +0800

    Add basic functionality

--- script.js
+++ script.js
@@ -1,31 +1,62 @@
 const gridSize = document.getElementById("grid-size");
 const gridContainer = document.getElementById("grid-container")
+const cells = document.getElementsByClassName("cell")
+const clear = document.getElementById("clear")
 
-
-const changeGridSize = (size) => {
-
-    for(let col = 0; col < size; col++){
-        let column = document.createElement("div")
-        column.setAttribute("data-col-num", `col${col}`)
-        column.style.cssText = "display:flex;"
-        gridContainer.appendChild(column)
-
-        for(let row = 0; row < size; row++){
-            column.appendChild(document.createElement('div'))
+const changeGridSize = (size, ref) => {
+    ref.nextElementSibling.textContent = `${size}x${size}`
+    for(let row = 0; row < size; row++){
+        const rows = document.createElement("div")
+        gridContainer.appendChild(rows)
+        for(let col = 0; col < size; col++){
+            const column = document.createElement("div")
+            column.setAttribute("class", "cell")
+            rows.appendChild(column)
         }
     }
 }
 
 const removeGrid = () => {
-    let grids = document.querySelectorAll("#grid-container div")
+    const grids = document.querySelectorAll("#grid-container div")
     for(let grid of grids){
         grid.remove()
-    }
+    }   
 }
 
-window.addEventListener("load", changeGridSize(gridSize.value))
+const addChangeColorInHover = (cells, color="#262626") => {
+    Array.from(cells).forEach(cell => cell.addEventListener("mouseenter", (e) => {
+        e.target.style.backgroundColor = color;
+    }))
+}
 
-gridSize.addEventListener("change", (e)=>{
+const pickColor = () => {
+    return false
+}
+
+const clearColor = () => {
+    Array.from(cells).forEach(cell => cell.style.backgroundColor = "#fff")
+}
+
+//on window load
+const start = () => {
+
+    window.addEventListener("load", () => {
+        changeGridSize(gridSize.value, gridSize)
+    })
+    
+    
+    window.addEventListener("load", () => {
+        addChangeColorInHover(cells)
+    })
+}
+start()
+
+
+gridSize.addEventListener("input", (e)=>{
     removeGrid()
-    changeGridSize(e.target.value)
+    changeGridSize(e.target.value, gridSize)
+    addChangeColorInHover(cells)
 })
+
+
+clear.addEventListener("click", clearColor)
